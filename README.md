3ccaa76b47f737e2075e5f27e49315db391fde97
Guilherme Reis <guilhermereis2009@hotmail.com>
2021-07-18 03:45:13 -0300

    Aplica todos os exercícios propostos no curso de desenvolvimento Android.
    Signed-off-by: Guilherme Reis <guilhermereis2009@hotmail.com>

--- README.md
+++ README.md
@@ -1,4 +1,11 @@
-# Understanding the Android Threading Model
+# Astronautas no espaço :space_invader:
 
-Exercício prático sobre os modelos de Threads no Android exemplificado com a implementação de AsyncTask
+App para Android que recupera de uma API e exibe os nomes de astronautas
+que estão nesse momento em alguma nave no espaço orbitanto a Terra.
 
+Aplica modelos de Threads no Android exemplificado com a implementação
+de AsyncTask.
+
+#### Screenshot
+
+![screenshot](screenshot.png)
--- app/src/main/java/com/everis/bootcamp/threading/MainActivity.kt
+++ app/src/main/java/com/everis/bootcamp/threading/MainActivity.kt
@@ -1,10 +1,9 @@
 package com.everis.bootcamp.threading
 
 import android.os.AsyncTask
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
-import androidx.core.view.isVisible
+import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -13,22 +12,47 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        //TODO: 018 - fazer o handle do clique do botão
+        button_load_data.setOnClickListener {
+            launchAstrosTask()
+        }
     }
 
+    fun showData(list: List<AstrosPeople>?) {
+        textview_data.text = ""
+        list?.forEach { people ->
+            textview_data.append("${people.name} - ${people.craft} \n\n")
+        }
+    }
 
-    //TODO: 013 - Criar função para exibir os dados carregados
+    fun showLoadingIndicator() {
+        progress_bar_load_indicator.visibility = View.VISIBLE
+    }
 
+    fun hideLoadingIndicator() {
+        progress_bar_load_indicator.visibility = View.GONE
+    }
 
-    //TODO: 014 - Criar função para exibir a ProgressBar
+    fun launchAstrosTask() {
+        val task = TaskAstros()
+        task.execute()
+    }
 
+    inner class TaskAstros : AsyncTask<Void, Int, List<AstrosPeople>>() {
+        private val repository = AstrosRepository()
 
-    //TODO: 015 - Criar função para esconder a ProgressBar
+        override fun onPreExecute() {
+            super.onPreExecute()
+            showLoadingIndicator()
+        }
 
+        override fun doInBackground(vararg params: Void?): List<AstrosPeople>? {
+            return repository.loadData()
+        }
 
-    //TODO: 017 - Criar função para lançar a Task
-
-
-    //TODO: 016 - Criar classe interna para rodar a tarefa assincrona
-
+        override fun onPostExecute(result: List<AstrosPeople>?) {
+            super.onPostExecute(result)
+            hideLoadingIndicator()
+            showData(result)
+        }
+    }
 }
